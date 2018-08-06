8b355f726b1514f8c38609a2231e325e408ca926
Pratheek Shanthraj <pshanthraj@Pratheeks-MacBook-Pro.local>
2018-08-06 01:25:58 +0100

    cleaning up thermal expansion

--- src/kinematics_thermal_expansion.f90
+++ src/kinematics_thermal_expansion.f90
@@ -11,23 +11,19 @@ module kinematics_thermal_expansion
  implicit none
  private
  integer(pInt),                       dimension(:),           allocatable,         public, protected :: &
-   kinematics_thermal_expansion_sizePostResults, &                                                                !< cumulative size of post results
-   kinematics_thermal_expansion_offset, &                                                                         !< which kinematics is my current damage mechanism?
-   kinematics_thermal_expansion_instance                                                                          !< instance of damage kinematics mechanism
+   kinematics_thermal_expansion_offset, &                                                                         !< which kinematics is my current mechanism?
+   kinematics_thermal_expansion_instance                                                                          !< instance of kinematics mechanism
 
- integer(pInt),                       dimension(:,:),         allocatable, target, public  :: &
-   kinematics_thermal_expansion_sizePostResult                                                                    !< size of each post result output
+ type, private :: tParameters                                                                                 !< container type for internal constitutive parameters
+   real(pReal) :: &                                                        
+     TRef = 300.0_pReal
+   real(pReal), dimension(3,3,3) :: &
+     alpha = 0.0_pReal
+                                                            
+ end type
 
- character(len=64),                   dimension(:,:),         allocatable, target, public  :: &
-   kinematics_thermal_expansion_output                                                                            !< name of each post result output
-   
- integer(pInt),                       dimension(:),           allocatable, target, public  :: &
-   kinematics_thermal_expansion_Noutput                                                                           !< number of outputs per instance of this damage 
+ type(tParameters), dimension(:), allocatable, private :: param                                       !< containers of constitutive parameters (len Ninstance)
 
-! enum, bind(c)                                                                                                   ! ToDo kinematics need state machinery to deal with sizePostResult
-!   enumerator :: undefined_ID, &                                                                                 ! possible remedy is to decouple having state vars from having output
-!                 thermalexpansionrate_ID                                                                         ! which means to separate user-defined types tState + tOutput...
-! end enum
  public :: &
    kinematics_thermal_expansion_init, &
    kinematics_thermal_expansion_initialStrain, &
@@ -66,9 +62,8 @@ subroutine kinematics_thermal_expansion_init(fileUnit)
  use material, only: &
    phase_kinematics, &
    phase_Nkinematics, &
-   phase_Noutput, &
-   KINEMATICS_thermal_expansion_label, &
-   KINEMATICS_thermal_expansion_ID
+   kinematics_thermal_expansion_label, &
+   kinematics_thermal_expansion_ID
  use config, only: &
    material_Nphase, &
    MATERIAL_partPhase
@@ -77,37 +72,33 @@ subroutine kinematics_thermal_expansion_init(fileUnit)
  integer(pInt), intent(in) :: fileUnit
 
  integer(pInt), allocatable, dimension(:) :: chunkPos
- integer(pInt) :: maxNinstance,phase,instance,kinematics
+ integer(pInt) :: maxNinstance,phase,instance,kinematics,i
  character(len=65536) :: &
    tag     = '', &
    line    = ''
 
- write(6,'(/,a)')   ' <<<+-  kinematics_'//KINEMATICS_thermal_expansion_LABEL//' init  -+>>>'
+ write(6,'(/,a)')   ' <<<+-  kinematics_'//kinematics_thermal_expansion_LABEL//' init  -+>>>'
  write(6,'(a15,a)') ' Current time: ',IO_timeStamp()
 #include "compilation_info.f90"
 
- maxNinstance = int(count(phase_kinematics == KINEMATICS_thermal_expansion_ID),pInt)
+ maxNinstance = int(count(phase_kinematics == kinematics_thermal_expansion_ID),pInt)
  if (maxNinstance == 0_pInt) return
  
  if (iand(debug_level(debug_constitutive),debug_levelBasic) /= 0_pInt) &
    write(6,'(a16,1x,i5,/)') '# instances:',maxNinstance
  
+ allocate(param(maxNinstance))
  allocate(kinematics_thermal_expansion_offset(material_Nphase), source=0_pInt)
  allocate(kinematics_thermal_expansion_instance(material_Nphase), source=0_pInt)
+
  do phase = 1, material_Nphase
    kinematics_thermal_expansion_instance(phase) = count(phase_kinematics(:,1:phase) == kinematics_thermal_expansion_ID)
    do kinematics = 1, phase_Nkinematics(phase)
      if (phase_kinematics(kinematics,phase) == kinematics_thermal_expansion_ID) &
        kinematics_thermal_expansion_offset(phase) = kinematics
-   enddo    
+   enddo
  enddo
-   
- allocate(kinematics_thermal_expansion_sizePostResults(maxNinstance),                     source=0_pInt)
- allocate(kinematics_thermal_expansion_sizePostResult(maxval(phase_Noutput),maxNinstance),source=0_pInt)
- allocate(kinematics_thermal_expansion_output(maxval(phase_Noutput),maxNinstance))
-          kinematics_thermal_expansion_output = ''
- allocate(kinematics_thermal_expansion_Noutput(maxNinstance),                             source=0_pInt) 
-
+ 
  rewind(fileUnit)
  phase = 0_pInt
  do while (trim(line) /= IO_EOF .and. IO_lc(IO_getTag(line,'<','>')) /= MATERIAL_partPhase)         ! wind forward to <phase>
@@ -125,20 +116,30 @@ subroutine kinematics_thermal_expansion_init(fileUnit)
      phase = phase + 1_pInt                                                                         ! advance phase section counter
      cycle                                                                                          ! skip to next line
    endif
-   if (phase > 0_pInt ) then; if (any(phase_kinematics(:,phase) == KINEMATICS_thermal_expansion_ID)) then         ! do not short-circuit here (.and. with next if statemen). It's not safe in Fortran
+   if (phase > 0_pInt ) then; if (any(phase_kinematics(:,phase) == kinematics_thermal_expansion_ID)) then         ! do not short-circuit here (.and. with next if statemen). It's not safe in Fortran
      instance = kinematics_thermal_expansion_instance(phase)                                                         ! which instance of my damage is present phase
      chunkPos = IO_stringPos(line)
      tag = IO_lc(IO_stringValue(line,chunkPos,1_pInt))                                             ! extract key...
      select case(tag)
-!       case ('(output)')
-!         output = IO_lc(IO_stringValue(line,chunkPos,2_pInt))                                       ! ...and corresponding output
-!         select case(output)
-!           case ('thermalexpansionrate')
-!             kinematics_thermal_expansion_Noutput(instance) = kinematics_thermal_expansion_Noutput(instance) + 1_pInt
-!             kinematics_thermal_expansion_outputID(kinematics_thermal_expansion_Noutput(instance),instance) = &
-!               thermalexpansionrate_ID
-!             kinematics_thermal_expansion_output(kinematics_thermal_expansion_Noutput(instance),instance) = output
-! ToDo add sizePostResult loop afterwards...
+       case ('thermal_expansion11')
+         do i = 2_pInt, min(4,chunkPos(1))                                                        ! read up to three parameters (constant, linear, quadratic with T)
+           param(instance)%alpha(1,1,i-1) = IO_floatValue(line,chunkPos,i)
+         enddo
+
+       case ('thermal_expansion22')
+         do i = 2_pInt, min(4,chunkPos(1))                                                        ! read up to three parameters (constant, linear, quadratic with T)
+           param(instance)%alpha(2,2,i-1) = IO_floatValue(line,chunkPos,i)
+         enddo
+
+       case ('thermal_expansion33')
+         do i = 2_pInt, min(4,chunkPos(1))                                                        ! read up to three parameters (constant, linear, quadratic with T)
+           param(instance)%alpha(3,3,i-1) = IO_floatValue(line,chunkPos,i)
+         enddo
+
+       case ('reference_temperature')
+         param(instance)%TRef = IO_floatValue(line,chunkPos,2)
+
+       case default
 
      end select
    endif; endif
@@ -154,9 +155,6 @@ pure function kinematics_thermal_expansion_initialStrain(ipc, ip, el)
    material_phase, &
    temperature, &
    thermalMapping
- use lattice, only: &
-   lattice_thermalExpansion33, &
-   lattice_referenceTemperature
  
  implicit none
  integer(pInt), intent(in) :: &
@@ -167,18 +165,20 @@ pure function kinematics_thermal_expansion_initialStrain(ipc, ip, el)
    kinematics_thermal_expansion_initialStrain                                                       !< initial thermal strain (should be small strain, though)
  integer(pInt) :: &
    phase, &
+   instance, &
    offset
    
  phase  = material_phase(ipc,ip,el)
+ instance = kinematics_thermal_expansion_instance(phase)
  offset = thermalMapping(phase)%p(ipc,ip,el)
  
  kinematics_thermal_expansion_initialStrain = &
-   (temperature(phase)%p(offset) - lattice_referenceTemperature(phase))**1 / 1. * &
-   lattice_thermalExpansion33(1:3,1:3,1,phase) + &                                                  ! constant  coefficient
-   (temperature(phase)%p(offset) - lattice_referenceTemperature(phase))**2 / 2. * &
-   lattice_thermalExpansion33(1:3,1:3,2,phase) + &                                                  ! linear    coefficient
-   (temperature(phase)%p(offset) - lattice_referenceTemperature(phase))**3 / 3. * &
-   lattice_thermalExpansion33(1:3,1:3,3,phase)                                                      ! quadratic coefficient
+   (temperature(phase)%p(offset) - param(instance)%TRef)**1 / 1. * &
+   param(instance)%alpha(1:3,1:3,1) + &                                                  ! constant  coefficient
+   (temperature(phase)%p(offset) - param(instance)%TRef)**2 / 2. * &
+   param(instance)%alpha(1:3,1:3,2) + &                                                  ! linear    coefficient
+   (temperature(phase)%p(offset) - param(instance)%TRef)**3 / 3. * &
+   param(instance)%alpha(1:3,1:3,3)                                                      ! quadratic coefficient
   
 end function kinematics_thermal_expansion_initialStrain
 
@@ -191,9 +191,6 @@ subroutine kinematics_thermal_expansion_LiAndItsTangent(Li, dLi_dTstar3333, ipc,
    temperature, &
    temperatureRate, &
    thermalMapping
- use lattice, only: &
-   lattice_thermalExpansion33, &
-   lattice_referenceTemperature
  
  implicit none
  integer(pInt), intent(in) :: &
@@ -206,25 +203,27 @@ subroutine kinematics_thermal_expansion_LiAndItsTangent(Li, dLi_dTstar3333, ipc,
    dLi_dTstar3333                                                                                   !< derivative of Li with respect to Tstar (4th-order tensor defined to be zero)
  integer(pInt) :: &
    phase, &
+   instance, &
    offset
  real(pReal) :: &
    T, TRef, TDot  
    
  phase = material_phase(ipc,ip,el)
+ instance = kinematics_thermal_expansion_instance(phase)
  offset = thermalMapping(phase)%p(ipc,ip,el)
  T = temperature(phase)%p(offset)
  TDot = temperatureRate(phase)%p(offset)
- TRef = lattice_referenceTemperature(phase)
+ TRef = param(instance)%TRef
  
  Li = TDot * ( &
-               lattice_thermalExpansion33(1:3,1:3,1,phase)*(T - TRef)**0 &                           ! constant  coefficient
-             + lattice_thermalExpansion33(1:3,1:3,2,phase)*(T - TRef)**1 &                           ! linear    coefficient
-             + lattice_thermalExpansion33(1:3,1:3,3,phase)*(T - TRef)**2 &                           ! quadratic coefficient
+               param(instance)%alpha(1:3,1:3,1)*(T - TRef)**0 &                           ! constant  coefficient
+             + param(instance)%alpha(1:3,1:3,2)*(T - TRef)**1 &                           ! linear    coefficient
+             + param(instance)%alpha(1:3,1:3,3)*(T - TRef)**2 &                           ! quadratic coefficient
              ) / &
       (1.0_pReal \
-            + lattice_thermalExpansion33(1:3,1:3,1,phase)*(T - TRef)**1 / 1. &
-            + lattice_thermalExpansion33(1:3,1:3,2,phase)*(T - TRef)**2 / 2. &
-            + lattice_thermalExpansion33(1:3,1:3,3,phase)*(T - TRef)**3 / 3. &
+            + param(instance)%alpha(1:3,1:3,1)*(T - TRef)**1 / 1. &
+            + param(instance)%alpha(1:3,1:3,2)*(T - TRef)**2 / 2. &
+            + param(instance)%alpha(1:3,1:3,3)*(T - TRef)**3 / 3. &
       )
  dLi_dTstar3333 = 0.0_pReal 
   
